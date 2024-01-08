b3300b35fd99adbf39a3203c7154e79daa83d586
João Paulo Peres <peresjoao244@gmail.com>
2024-01-08 20:04:54 -0300

    feat: Create Content02

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Header from "./components/Header";
 import Presentation from "./components/Presentation";
 import Section01 from "./components/section01";
+import Section02 from "./components/section02";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Presentation />
       <About />
       <Section01 />
+      <Section02 />
     </div>
   );
 }
--- src/components/Content01/index.jsx
+++ src/components/Content01/index.jsx
@@ -1,13 +1,13 @@
-import './style.css'
+import "./style.css";
 export default function Content01({ title, img, description, first }) {
   const styleContent = {
-    background: first ? '#f4f4f4' : '#f7f7f7'
-  }
+    background: first ? "#f4f4f4" : "#f7f7f7",
+  };
   return (
     <div style={styleContent} className="content01">
       <h3>{title}</h3>
       <img src={img} alt={title} />
       <p>{description}</p>
     </div>
-  )
+  );
 }
